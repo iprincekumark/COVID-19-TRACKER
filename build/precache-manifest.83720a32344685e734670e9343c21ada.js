c1cfe17cbbc4d3d212e607d9c5e26828f58164cc
Prince Kumar Kanoujiya <iprincekumark@gmail.com>
2020-11-07 15:58:10 +0530

    Covid-19 Tracker

---
+++ build/precache-manifest.83720a32344685e734670e9343c21ada.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "522f687cef2aee74579ede6f106ec57c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ae31a74c186d8f8509e",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "cab5348ab68c3dbac350",
+    "url": "/static/css/main.6e5005c2.chunk.css"
+  },
+  {
+    "revision": "4ae31a74c186d8f8509e",
+    "url": "/static/js/2.f675a1dd.chunk.js"
+  },
+  {
+    "revision": "af5492f10234ec8ba29a624c4fe6fb11",
+    "url": "/static/js/2.f675a1dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cab5348ab68c3dbac350",
+    "url": "/static/js/main.a344c72c.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
